6009c6c262fceb69307592a57a4425722474448e
Caio Ribeiro <cacobribeiro@gmail.com>
2021-10-22 15:16:43 -0300

    controller da containers

---
+++ src/controllers/containers.js
@@ -0,0 +1,67 @@
+const { Container } = require('../../models');
+
+module.exports = {
+    get: async (req, res) => {
+        try {
+            const { containerId } = req.body;
+
+            const containerInfo = await Container.findOne({ where: { containerId } });
+
+            if (!containerInfo) return res.status(404).send({ error: 'not found' });
+
+            res.status(200).send({ containerInfo });
+        } catch (err) {
+            res.status(400).json({ message: err });
+        }
+    },
+
+    post: async (req, res) => {
+        try {
+            const { fullName, containerId, type, status, category } = req.body;
+
+            const verifyContainer = await Container.findOne({ where: { containerId } });
+
+            if (verifyContainer)
+                return res.status(401).send({ message: 'Já existe este containerId.' });
+
+            const containerInfo = await Container.create({
+                fullName,
+                containerId,
+                type,
+                status,
+                category,
+            });
+
+            res.status(200).send({ containerInfo });
+        } catch (err) {
+            res.status(400).json({ message: err });
+        }
+    },
+
+    patch: async (req, res) => {
+        try {
+            const { containerId } = req.body;
+
+            const containerInfo = await Container.update(
+                { ...req.body },
+                { where: { containerId } }
+            );
+
+            res.status(200).send({ containerInfo });
+        } catch (err) {
+            res.status(400).json({ message: err });
+        }
+    },
+
+    delete: async (req, res) => {
+        try {
+            const { containerId } = req.body;
+
+            const containerInfo = await Container.destroy({ where: { containerId } });
+
+            res.status(200).send({ containerInfo });
+        } catch (err) {
+            res.status(400).json({ message: err });
+        }
+    },
+};
