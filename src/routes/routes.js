72dab1f3f47182e0d8a200d89684dd9eae445edc
Caio Ribeiro <cacobribeiro@gmail.com>
2021-10-22 15:15:34 -0300

    routes automaticas criada

---
+++ src/routes/routes.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+require('dotenv').config();
+
+module.exports = (app) => {
+  const PORT = process.env.PORT || 3000;
+
+  console.log('\nRodas Disponiveis:\n');
+  fs.readdirSync('./src/routes/').forEach((fileName) => {
+    if (fileName.includes('routes')) return;
+
+    let route = '/api/' + fileName.split('.')[0];
+
+    console.log(`> http://localhost:${PORT}${route}\n`);
+    
+    app.use(route, require('./' + fileName));
+  });
+
+  return;
+};
